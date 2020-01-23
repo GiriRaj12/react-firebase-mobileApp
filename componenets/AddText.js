35290fdcea9561978c8b41994dc84b54b3d1786b
Giriraj <giriraj.sekar@anywhere.co>
2020-01-23 22:38:15 +0530

    Added API

--- componenets/AddText.js
+++ componenets/AddText.js
@@ -3,16 +3,31 @@ import { View, Picker, Text } from 'react-native'
 import styles from './styles'
 import { TextInput } from 'react-native-gesture-handler'
 import CustomButton from '../cutomComponenets/CustomButton'
-import content  from '../commonContent/content.js'
+import content from '../commonContent/content.js'
+import TextApi from '../config/textApi'
 export default class AddText extends React.Component {
-    state = {
-        fromLanguage: "",
-        toLanguage: "",
-        inputText: ""
-    }
+        state = {
+            fromLanguage: "",
+            toLanguage: "",
+            inputText: "",
+            textDisplay:<Text></Text>
+        }
     addText() {
         let url = "https://translation.googleapis.com/language/translate/v2";
-        console.log(this.state.toLanguage+"");
+
+    }
+    handleFromChange(value) {
+        console.log(value);
+        this.setState({ fromLanguage: value });
+    }
+    
+    handleToChange(value) {
+        console.log(value)
+        this.setState({ toLanguage: value });
+    }
+
+    translate(lang){
+        var url = TextApi.url+"?key="+TextApi.key+",lang="+
     }
 
     render() {
@@ -20,47 +35,47 @@ export default class AddText extends React.Component {
             <View style={styles.appbackground}>
                 <View style={{ width: '100%', marginTop: 80 }}>
                     <View style={styles.parralelViewParent}>
-                        <View style={{marginLeft:40}}>
+                        <View style={{ marginLeft: 40 }}>
                             <Text style={{ color: '#ff8c00', fontSize: 20, fontWeight: "bold" }}>From </Text>
                             <Picker
                                 selectedValue={this.state.fromLanguage}
                                 style={{ height: 50, width: 140 }}
-                                onValueChange={(itemValue, itemIndex) => this.setState({ fromLanguage : itemValue })}>
-                                <Picker.Item label="English" value="english"/>
-                                <Picker.Item label="Tamil" value="tamil" />
-                                <Picker.Item label="Spanish" value="spanish"/>
+                                onValueChange={this.handleFromChange}>
+                                <Picker.Item label="English" value="en" />
+                                <Picker.Item label="Tamil" value="ta" />
+                                <Picker.Item label="Spanish" value="es" />
                             </Picker>
-                        </View>           
-                        <View style={{marginLeft:40}}>
-                        <Text style={{ color: '#ff8c00', fontSize: 20, fontWeight: "bold" }}>To </Text>
+                        </View>
+                        <View style={{ marginLeft: 40 }}>
+                            <Text style={{ color: '#ff8c00', fontSize: 20, fontWeight: "bold" }}>To </Text>
                             <Picker
                                 selectedValue={this.state.toLanguage}
                                 style={{ height: 50, width: 140 }}
-                                onValueChange={(itemValue, itemIndex) => this.setState({ toLanguage : itemValue })}>
-                                <Picker.Item label="English" value="eng" />
-                                <Picker.Item label="Tamil" value="tamil" />
-                                <Picker.Item label="Spanish" value="spanish"/>
+                                onValueChange={this.handleToChange}>
+                                <Picker.Item label="English" value="en" />
+                                <Picker.Item label="Tamil" value="ta" />
+                                <Picker.Item label="Spanish" value="es" />
                             </Picker>
                         </View>
                     </View>
-                    <View style={styles.emptyspace}/>
+                    <View style={styles.emptyspace} />
                     <TextInput style={styles.inputtext}
                         placeholder="Add Translated Text"
                         onChangeText={(text) => this.setState({ inputText: text })}
                     />
-                    <View style={styles.emptyspace}/>
-                    <CustomButton callback={this.addText} text="Add Text" styles={styles.normalbutton}>
+                    <View style={styles.emptyspace} />
+                    <CustomButton callback={this.addText(this.state.fromLanguage, this.state.toLanguage)} text="Add Text" styles={styles.normalbutton}>
                     </CustomButton>
-                    <View style={styles.emptyspace}/>
-                    <View style={styles.emptyspace}/>
-                    <Text style={{color:'black', marginLeft:20,marginTop:40}}>Instructions</Text>
+                    <View style={styles.emptyspace} />
+                    <View style={styles.emptyspace} />
+                    <Text style={{ color: 'black', marginLeft: 20, marginTop: 40 }}>Instructions</Text>
                     <Text style={styles.instructionsText}>
-                    {content.addTextContent}
+                        {content.addTextContent}
                     </Text>
-                    <View style={styles.emptyspace}/>
-                    <Text style={{color:'black', marginLeft:20}}>Example</Text>
-                    <Text  style={styles.instructionsText}>
-                    {content.addTextExample}
+                    <View style={styles.emptyspace} />
+                    <Text style={{ color: 'black', marginLeft: 20 }}>Example</Text>
+                    <Text style={styles.instructionsText}>
+                        {content.addTextExample}
                     </Text>
                 </View>
             </View>
--- componenets/MainPage.js
+++ componenets/MainPage.js
@@ -1,16 +1,16 @@
 import React from 'react'
-import {Image, ScrollView,TouchableOpacity } from 'react-native';
+import { Image, ScrollView, TouchableOpacity } from 'react-native';
 import styles from './styles';
 
-class MainPage extends React.Component{
-    render(){
-        return(
+class MainPage extends React.Component {
+    render() {
+        return (
             <ScrollView style={styles.appbackground}>
-            <TouchableOpacity style={styles.floatingButtonTouch}
-                    onPress={()=>this.props.navigation.navigate('AddText')}>
-                        <Image source={{uri : 'https://reactnativecode.com/wp-content/uploads/2017/11/Floating_Button.png'}} 
-                            style={styles.floatingButton} />
-            </TouchableOpacity>
+                <TouchableOpacity style={styles.floatingButtonTouch}
+                    onPress={() => this.props.navigation.navigate('AddText')}>
+                    <Image source={{ uri: 'https://reactnativecode.com/wp-content/uploads/2017/11/Floating_Button.png' }}
+                        style={styles.floatingButton} />
+                </TouchableOpacity>
             </ScrollView>
         );
     }
